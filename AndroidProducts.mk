8e21bbe486d2429f9ec53077b97ceb8c08e42194
GoRhanHee <gorhanhee01@gmail.com>
2026-03-02 23:47:39 +0900

    rename omni to twrp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_c2q.mk
+    $(LOCAL_DIR)/twrp_c2q.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_c2q-user \
-    omni_c2q-userdebug \
-    omni_c2q-eng
+    twrp_c2q-user \
+    twrp_c2q-userdebug \
+    twrp_c2q-eng
